312c4e7467b109675bffb5dbe043f2a64926699d
Vsevolod_S <seva.smirnov2014@gmail.com>
2023-05-23 22:22:24 +0300

    first version of YouTube_API

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,11 @@
+import json
+import os
+
+from googleapiclient.discovery import build
+
+import isodate
+api_key: str = os.getenv('API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
@@ -5,8 +13,12 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
-    def print_info(self) -> None:
+    def print_info(self, channel_id) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+moscowpython.print_info(moscowpython.channel_id)
